ec590390fd4227a992c1d9d3d14c91f04a1bb67c
Alex <alexlmurphy@yahoo.com>
2021-09-01 15:27:29 -0400

    Fix js errors.

--- AM_gallery.js
+++ AM_gallery.js
@@ -1,14 +1,14 @@
 function showImage(self) {
 	if (self.isMagnified == null || self.isMagnified == false) {
 		self.isMagnified = true;
-		self.style.width = String(window.innerWidth - 100) + "px";
 		self.marginOld = self.style.margin;
+		self.widthOld = self.style.width;
 		self.style.width = "100%";
 		self.style.margin = "0 0 0 0";
 		
 	} else {	
-		self.style.width = "65%";
-		self.isMagnified = false;
+		self.style.width = self.widthOld;
 		self.style.margin = self.marginOld;
+		self.isMagnified = false;
 	}
 }
